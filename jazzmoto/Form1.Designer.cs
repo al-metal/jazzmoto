08f79f46289b5daa8775b53136c98f8c6995b1d8
al-metal <al-metal@users.noreply.github.com>
2017-06-23 15:01:13 +0400

    Загрузка формы

--- jazzmoto/Form1.Designer.cs
+++ jazzmoto/Form1.Designer.cs
@@ -28,20 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSaveTemplate = new System.Windows.Forms.Button();
+            this.btnImages = new System.Windows.Forms.Button();
+            this.tbKeywords = new System.Windows.Forms.TextBox();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.rtbFullText = new System.Windows.Forms.RichTextBox();
+            this.rtbMiniText = new System.Windows.Forms.RichTextBox();
+            this.btnActual = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbLoginNethouse = new System.Windows.Forms.TextBox();
+            this.tbPassNethouse = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnSaveTemplate
+            // 
+            this.btnSaveTemplate.Location = new System.Drawing.Point(575, 139);
+            this.btnSaveTemplate.Name = "btnSaveTemplate";
+            this.btnSaveTemplate.Size = new System.Drawing.Size(218, 23);
+            this.btnSaveTemplate.TabIndex = 21;
+            this.btnSaveTemplate.Text = "Сохранить шаблон";
+            this.btnSaveTemplate.UseVisualStyleBackColor = true;
+            this.btnSaveTemplate.Click += new System.EventHandler(this.btnSaveTemplate_Click);
+            // 
+            // btnImages
+            // 
+            this.btnImages.Location = new System.Drawing.Point(575, 110);
+            this.btnImages.Name = "btnImages";
+            this.btnImages.Size = new System.Drawing.Size(218, 23);
+            this.btnImages.TabIndex = 20;
+            this.btnImages.Text = "Обработать картинки";
+            this.btnImages.UseVisualStyleBackColor = true;
+            // 
+            // tbKeywords
+            // 
+            this.tbKeywords.Location = new System.Drawing.Point(12, 308);
+            this.tbKeywords.Name = "tbKeywords";
+            this.tbKeywords.Size = new System.Drawing.Size(557, 20);
+            this.tbKeywords.TabIndex = 19;
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(12, 282);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(557, 20);
+            this.tbDescription.TabIndex = 18;
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(12, 256);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(557, 20);
+            this.tbTitle.TabIndex = 17;
+            // 
+            // rtbFullText
+            // 
+            this.rtbFullText.Location = new System.Drawing.Point(12, 134);
+            this.rtbFullText.Name = "rtbFullText";
+            this.rtbFullText.Size = new System.Drawing.Size(557, 116);
+            this.rtbFullText.TabIndex = 16;
+            this.rtbFullText.Text = "";
+            // 
+            // rtbMiniText
+            // 
+            this.rtbMiniText.Location = new System.Drawing.Point(12, 12);
+            this.rtbMiniText.Name = "rtbMiniText";
+            this.rtbMiniText.Size = new System.Drawing.Size(557, 116);
+            this.rtbMiniText.TabIndex = 15;
+            this.rtbMiniText.Text = "";
+            // 
+            // btnActual
+            // 
+            this.btnActual.Location = new System.Drawing.Point(575, 81);
+            this.btnActual.Name = "btnActual";
+            this.btnActual.Size = new System.Drawing.Size(218, 23);
+            this.btnActual.TabIndex = 14;
+            this.btnActual.Text = "Обработать сайт";
+            this.btnActual.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbLoginNethouse);
+            this.groupBox1.Controls.Add(this.tbPassNethouse);
+            this.groupBox1.Location = new System.Drawing.Point(575, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(218, 63);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "nethouse";
+            // 
+            // tbLoginNethouse
+            // 
+            this.tbLoginNethouse.Location = new System.Drawing.Point(6, 32);
+            this.tbLoginNethouse.Name = "tbLoginNethouse";
+            this.tbLoginNethouse.Size = new System.Drawing.Size(100, 20);
+            this.tbLoginNethouse.TabIndex = 0;
+            // 
+            // tbPassNethouse
+            // 
+            this.tbPassNethouse.Location = new System.Drawing.Point(112, 32);
+            this.tbPassNethouse.Name = "tbPassNethouse";
+            this.tbPassNethouse.PasswordChar = '*';
+            this.tbPassNethouse.Size = new System.Drawing.Size(100, 20);
+            this.tbPassNethouse.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(804, 339);
+            this.Controls.Add(this.btnSaveTemplate);
+            this.Controls.Add(this.btnImages);
+            this.Controls.Add(this.tbKeywords);
+            this.Controls.Add(this.tbDescription);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.rtbFullText);
+            this.Controls.Add(this.rtbMiniText);
+            this.Controls.Add(this.btnActual);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "JazzMoto";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSaveTemplate;
+        private System.Windows.Forms.Button btnImages;
+        private System.Windows.Forms.TextBox tbKeywords;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.RichTextBox rtbFullText;
+        private System.Windows.Forms.RichTextBox rtbMiniText;
+        private System.Windows.Forms.Button btnActual;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbLoginNethouse;
+        private System.Windows.Forms.TextBox tbPassNethouse;
     }
 }
 
--- jazzmoto/Form1.cs
+++ jazzmoto/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,122 @@ namespace jazzmoto
         public Form1()
         {
             InitializeComponent();
+            if (!Directory.Exists("files"))
+            {
+                Directory.CreateDirectory("files");
+            }
+            if (!Directory.Exists("pic"))
+            {
+                Directory.CreateDirectory("pic");
+            }
+
+            if (!File.Exists("files\\miniText.txt"))
+            {
+                File.Create("files\\miniText.txt");
+            }
+
+            if (!File.Exists("files\\fullText.txt"))
+            {
+                File.Create("files\\fullText.txt");
+            }
+
+            if (!File.Exists("files\\title.txt"))
+            {
+                File.Create("files\\title.txt");
+            }
+
+            if (!File.Exists("files\\description.txt"))
+            {
+                File.Create("files\\description.txt");
+            }
+
+            if (!File.Exists("files\\keywords.txt"))
+            {
+                File.Create("files\\keywords.txt");
+            }
+            StreamReader altText = new StreamReader("files\\miniText.txt", Encoding.GetEncoding("windows-1251"));
+            while (!altText.EndOfStream)
+            {
+                string str = altText.ReadLine();
+                rtbMiniText.AppendText(str + "\n");
+            }
+            altText.Close();
+
+            altText = new StreamReader("files\\fullText.txt", Encoding.GetEncoding("windows-1251"));
+            while (!altText.EndOfStream)
+            {
+                string str = altText.ReadLine();
+                rtbFullText.AppendText(str + "\n");
+            }
+            altText.Close();
+
+            altText = new StreamReader("files\\title.txt", Encoding.GetEncoding("windows-1251"));
+            while (!altText.EndOfStream)
+            {
+                string str = altText.ReadLine();
+                tbTitle.AppendText(str + "\n");
+            }
+            altText.Close();
+
+            altText = new StreamReader("files\\description.txt", Encoding.GetEncoding("windows-1251"));
+            while (!altText.EndOfStream)
+            {
+                string str = altText.ReadLine();
+                tbDescription.AppendText(str + "\n");
+            }
+            altText.Close();
+
+            altText = new StreamReader("files\\keywords.txt", Encoding.GetEncoding("windows-1251"));
+            while (!altText.EndOfStream)
+            {
+                string str = altText.ReadLine();
+                tbKeywords.AppendText(str + "\n");
+            }
+            altText.Close();
+        }
+
+        private void btnSaveTemplate_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            StreamWriter writers = new StreamWriter("files\\miniText.txt", false, Encoding.GetEncoding(1251));
+            count = rtbMiniText.Lines.Length;
+            for (int i = 0; rtbMiniText.Lines.Length > i; i++)
+            {
+                if (count - 1 == i)
+                {
+                    if (rtbFullText.Lines[i] == "")
+                        break;
+                }
+                writers.WriteLine(rtbMiniText.Lines[i].ToString());
+            }
+            writers.Close();
+
+            writers = new StreamWriter("files\\fullText.txt", false, Encoding.GetEncoding(1251));
+            count = rtbFullText.Lines.Length;
+            for (int i = 0; count > i; i++)
+            {
+                if (count - 1 == i)
+                {
+                    if (rtbFullText.Lines[i] == "")
+                        break;
+                }
+                writers.WriteLine(rtbFullText.Lines[i].ToString());
+            }
+            writers.Close();
+
+            writers = new StreamWriter("files\\title.txt", false, Encoding.GetEncoding(1251));
+            writers.WriteLine(tbTitle.Lines[0]);
+            writers.Close();
+
+            writers = new StreamWriter("files\\description.txt", false, Encoding.GetEncoding(1251));
+            writers.WriteLine(tbDescription.Lines[0]);
+            writers.Close();
+
+            writers = new StreamWriter("files\\keywords.txt", false, Encoding.GetEncoding(1251));
+            writers.WriteLine(tbKeywords.Lines[0]);
+            writers.Close();
+
+            MessageBox.Show("Сохранено");
         }
     }
 }
